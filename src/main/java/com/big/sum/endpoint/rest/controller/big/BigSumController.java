d690b69dde7a71db7967bbb4e06341cfc35c1639
Nantenaina <128221497+Nante1805@users.noreply.github.com>
2023-12-22 09:58:53 +0300

    fix : big sum

--- src/main/java/com/big/sum/endpoint/rest/controller/big/BigSumController.java
+++ src/main/java/com/big/sum/endpoint/rest/controller/big/BigSumController.java
@@ -1,2 +1,22 @@
-package com.big.sum.endpoint.rest.controller.big;public class BigSumController {
+package com.big.sum.endpoint.rest.controller.big;
+
+import com.big.sum.service.event.BigSumService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
+public class BigSumController {
+    private final BigSumService service;
+
+    public BigSumController(BigSumService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/big-sum")
+    public BigInteger add(@RequestParam(name = "a") String a, @RequestParam(name = "b") String b) {
+        return service.add(a, b);
+    }
 }
--- src/main/java/com/big/sum/service/event/BigSumService.java
+++ src/main/java/com/big/sum/service/event/BigSumService.java
@@ -1,4 +1,14 @@
 package com.big.sum.service.event;
 
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+@Service
 public class BigSumService {
+    public BigInteger add(String a, String b) {
+        BigInteger bigA = new BigInteger(a);
+        BigInteger bigB = new BigInteger(b);
+        return bigA.add(bigB);
+    }
 }
